f1b96bb02962de700f88878f5840318b89fa4594
Florent1711 <graphit01@hotmail.com>
2024-04-26 16:43:41 +0200

    Modif de 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,35 @@
+
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_hbnb():
+@app.route("/", strict_slashes=False)
+def hello():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
-def display_c(text):
-    # Replace underscores with spaces
-    formatted_text = text.replace('_', ' ')
-    return f"C {formatted_text}"
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    text = escape(text.replace("_", " "))
+    return "C {}".format(text)
 
 
-@app.route('/python/<text>')
-def display_python(text="is cool"):
-    # Replace underscores with spaces
-    formatted_text = text.replace('_', ' ')
-    return f"Python {formatted_text}"
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    text = escape(text.replace("_", " "))
+    return "Python {}".format(text)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
+    app.run(host='0.0.0.0', port=5000)
