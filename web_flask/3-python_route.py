2b448c029df8f8fc854e21c831868dd2f2aa0abf
Florent1711 <graphit01@hotmail.com>
2024-04-26 16:39:43 +0200

    Modif de 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-"""Starts a Flask web application"""
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,24 +8,24 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def custom_text(text="is cool"):
+@app.route('/c/<text>')
+def display_c(text):
     # Replace underscores with spaces
     formatted_text = text.replace('_', ' ')
     return f"C {formatted_text}"
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is cool"):
+@app.route('/python/<text>')
+def display_python(text="is cool"):
     # Replace underscores with spaces
     formatted_text = text.replace('_', ' ')
     return f"Python {formatted_text}"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
